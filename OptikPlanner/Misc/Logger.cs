b9b25a50529174b69287257ffb776c56a3734384
Pilsneren <Parbst2600@gmail.com>
2016-11-18 10:37:44 +0100

    MidwayLogging

--- OptikPlanner/Misc/Logger.cs
+++ OptikPlanner/Misc/Logger.cs
@@ -5,34 +5,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OptikPlanner.View;
+using System.Diagnostics;
 
 namespace OptikPlanner.Misc
 {
     class Logger
     {
         private static StreamWriter swLog;
-        private const string LOG_FILE_PATH = @"C:\Users\Daniel\Desktop\CancelAppointmentLog.txt";
+        private const string LOG_FILE_PATH = @"C:\Users\Daniel\Desktop\";
+        private const string source = "OptikPlannerLog";
 
         static Logger()
         {
-            Logger.OpenLogger();
+            //Logger.OpenLogger();
+            System.Diagnostics.EventLog OptikPlannerEvent = new System.Diagnostics.EventLog();
+            OptikPlannerEvent.Source = source;
         }
 
-        public static void OpenLogger()
-        {
-            Logger.swLog = new StreamWriter(LOG_FILE_PATH, false);
-            Logger.swLog.AutoFlush = true;
-        }
+        //public static void OpenLogger()
+        //{
+        //    Logger.swLog = new StreamWriter(LOG_FILE_PATH, false);
+        //    Logger.swLog.AutoFlush = true;
+        //}
         public static void LogThisLine(string sLogLine)
         {
-            Logger.swLog.WriteLine(DateTime.Now.ToLongDateString() + " " + sLogLine);
-            Logger.swLog.Flush();
-        }
+            Logger.swLog.WriteLine("\n" + DateTime.Now.ToLongDateString() + " " + sLogLine);
+            if (!EventLog.SourceExists(source))
+                EventLog.CreateEventSource(source, sLogLine);
 
-        public static void CloseLogger()
-        {
-            Logger.swLog.Flush();
-            Logger.swLog.Close();
+            EventLog.WriteEntry(source, DateTime.Now.ToLongDateString() + " " + sLogLine);
         }
+        // Logger.swLog.Flush();
     }
+
+        //public static void CloseLogger()
+        //{
+        //    Logger.swLog.Flush();
+        //    Logger.swLog.Close();
+        //}
+    //}
 }
