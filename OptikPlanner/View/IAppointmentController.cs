82e601f8cf3727002d4344079638021717507e41
Andreas <andreas@adaptmobile.dk>
2016-11-18 09:53:05 +0100

    N commit

---
+++ OptikPlanner/View/IAppointmentController.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms.Calendar;
+using OptikPlanner.Controller;
+
+namespace OptikPlanner.View
+
+{
+    public interface IAppointmentView
+    {
+        Calendar Calendar { get; }
+
+        void SetController(CreateAppointmentController controller);
+    }
+}
