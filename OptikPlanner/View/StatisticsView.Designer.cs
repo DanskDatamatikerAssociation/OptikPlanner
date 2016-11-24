2afe94f465e068deb7423697016fd79d50bbc206
Andreas <andreas@adaptmobile.dk>
2016-11-24 10:09:58 +0100

    StatisticsView 1.1

---
+++ OptikPlanner/View/StatisticsView.Designer.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using OptikPlanner.Model;
+
+namespace OptikPlanner.View
+{
+    partial class StatisticsView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.DatoLabel = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Aflysninger",
+            "Lokaler",
+            "Medarbejdere"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(119, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 94);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(119, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // DatoLabel
+            // 
+            this.DatoLabel.AutoSize = true;
+            this.DatoLabel.Location = new System.Drawing.Point(14, 71);
+            this.DatoLabel.Name = "DatoLabel";
+            this.DatoLabel.Size = new System.Drawing.Size(30, 13);
+            this.DatoLabel.TabIndex = 8;
+            this.DatoLabel.Text = "Dato";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Alle"});
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 146);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 49);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Alle",
+            "Uge 1",
+            "Uge 2",
+            "Uge 3"});
+            this.checkedListBox2.Location = new System.Drawing.Point(13, 219);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(120, 49);
+            this.checkedListBox2.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Vælg medarbejder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Vælg uge";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Type";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(208, 37);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(355, 231);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // StatisticsView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 283);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.DatoLabel);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "StatisticsView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "StatisticsView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+   
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private Label DatoLabel;
+        private CheckedListBox checkedListBox1;
+        private CheckedListBox checkedListBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ListView listView1;
+    }
+}
---
+++ OptikPlanner/View/StatisticsView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptikPlanner.View
+{
+    public partial class StatisticsView : Form
+    {
+        public StatisticsView()
+        {
+            InitializeComponent();
+
+            
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "MM/yyyy";
+            dateTimePicker1.ShowUpDown = true;
+
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                listView1.Columns.Clear();
+                listView1.Columns.Add("Aflysninger", 155);
+                listView1.Columns.Add("Mødte ikke op", 100);
+                listView1.Columns.Add("Telefon", 100);
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                listView1.Columns.Clear();
+                listView1.Columns.Add("Lokale", 155);
+                listView1.Columns.Add("Type", 100);
+                listView1.Columns.Add("Tilgængelighed", 100);
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                listView1.Columns.Clear();
+                listView1.Columns.Add("Navn", 155);
+                listView1.Columns.Add("Mødte ikke op", 100);
+                listView1.Columns.Add("Opkald", 100);
+
+            }
+
+        }
+
+    }
+}
