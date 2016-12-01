d0e18da227dcf7451861a99f5c60d510ac3bde3d
Andreas <andreas@adaptmobile.dk>
2016-12-01 12:25:54 +0100

    employee_availability

--- OptikPlanner/Controller/StatisticsViewController.cs
+++ OptikPlanner/Controller/StatisticsViewController.cs
@@ -15,7 +15,6 @@ namespace OptikPlanner.Controller
         OptikItDbContext db = new OptikItDbContext();
         CancelAppointmentController controller = new CancelAppointmentController();
         
-       
 
         public int TotalCancelStatistics()
         {
@@ -26,13 +25,13 @@ namespace OptikPlanner.Controller
             return dic + dic1 + dic2;
         }
 
-        public void NoShowStatistics()
-        {
-            foreach (var s in controller.noShowDic)
-            {
-                test[0].SubItems[0].Text = s.Value;
-            }
-        }
+        //public void NoShowStatistics()
+        //{
+        //    foreach (var s in controller.noShowDic)
+        //    {
+        //        test[0].SubItems[0].Text = s.Value;
+        //    }
+        //}
 
         public void PhoneCancelStatistics()
         {
@@ -43,21 +42,6 @@ namespace OptikPlanner.Controller
         {
             
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         public EYEEXAMROOMS GetRooms()
@@ -75,30 +59,50 @@ namespace OptikPlanner.Controller
 
         }
 
-        public APTDETAILS GetAppointments()
+        public List<APTDETAILS> GetAppointments()
         {
-            
+            List<APTDETAILS> appointments = new List<APTDETAILS>();
             var customer = GetCustomer();
 
-            USERS user = GetUser();
+            List<USERS>  user = GetUser();
             EYEEXAMROOMS room = GetRooms();
 
-            APTDETAILS appointment = new APTDETAILS(10, user, room, DateTime.Now, "12:15", "12:30", customer, "Test aftale");
+            APTDETAILS appointment = new APTDETAILS(10, user[0], room, DateTime.Now, "12:15", "12:30", customer, "Test aftale");
+            APTDETAILS appointment1 = new APTDETAILS(10, user[1], room, DateTime.Now, "12:15", "12:30", customer, "Test aftale");
+            APTDETAILS appointment2 = new APTDETAILS(10, user[2], room, DateTime.Now, "12:15", "12:30", customer, "Test aftale");
 
-            return appointment;
+            appointments.Add(appointment);
+            appointments.Add(appointment1);
+            appointments.Add(appointment2);
+            return appointments;
         }
 
 
 
-        public USERS GetUser()
+        public List<USERS> GetUser()
         {
             USERS user = new USERS();
             user.US_CPRNO = "2001927891";
             user.US_USERNAME = "MyUserName";
             user.US_STAMP = 1;
 
+            USERS user1 = new USERS();
+            user.US_CPRNO = "2001927892";
+            user.US_USERNAME = "MyUser";
+            user.US_STAMP = 2;
 
-            return user;
+            USERS user2 = new USERS();
+            user.US_CPRNO = "2001927893";
+            user.US_USERNAME = "MyUserHEJ";
+            user.US_STAMP = 3;
+
+            List<USERS> users = new List<USERS>();
+
+            users.Add(user);
+            users.Add(user1);
+            users.Add(user2);
+
+            return users;
         }
 
         public CUSTOMERS GetCustomer()
@@ -110,6 +114,20 @@ namespace OptikPlanner.Controller
 
 
             return customer;
+        }
+
+        public List<APTDETAILS> GetUserAppointments(USERS user)
+        {
+            List<APTDETAILS> appointmentsByUser = new List<APTDETAILS>();
+            var appointments = GetAppointments();
+
+            foreach (var a in appointments)
+            {
+                if (a.APD_USER == user.US_STAMP) appointmentsByUser.Add(a);
+            }
+
+            return appointmentsByUser;
+
         }
     }
 }
--- OptikPlanner/View/StatisticsView.cs
+++ OptikPlanner/View/StatisticsView.cs
@@ -2,28 +2,114 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Infrastructure;
 using System.Drawing;
+using System.Drawing.Text;
+using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OptikPlanner.Controller;
+using OptikPlanner.Model;
 
 namespace OptikPlanner.View
 {
     public partial class StatisticsView : Form
     {
+        private int totalMinutes = 0;
+        private int totalAvailibity = new int();
+        private int availibilityLeft = new int();
+        private List<USERS> employees = new List<USERS>();
+        private StatisticsViewController controller = new StatisticsViewController(); 
+        private List<APTDETAILS> appointments = new List<APTDETAILS>(); 
+        private int totalHours = 0;
+
         public StatisticsView()
         {
             InitializeComponent();
-
-            
+  
             dateTimePicker1.Format = DateTimePickerFormat.Custom;
             dateTimePicker1.CustomFormat = "MM/yyyy";
             dateTimePicker1.ShowUpDown = true;
+            //CreateAppointments();
+            GetUserAvailability();
+            
+        }
+
+        //public void CreateAppointments()
+        //{
+        //    employees = controller.GetUser();
+        //    foreach (var user in employees)
+        //    {
+        //        appointments = controller.GetUserAppointments(user);
+        //    }
+            
+
+        //    foreach (APTDETAILS e in appointments)
+        //    {
+        //        DateTime timeTo = DateTime.Parse(e.APD_TIMETO);
+        //        DateTime timeFrom = DateTime.Parse(e.APD_TIMEFROM);
+
+        //        TimeSpan totalTime = timeTo.Subtract(timeFrom);
+
+        //        totalMinutes = (int)totalTime.TotalMinutes;
+        //    }
+        //}
+
+        public void GetUserAvailability()
+        {
+            totalMinutes = new int();
+            totalHours = new int();
+            totalAvailibity = 142;
+            employees = controller.GetUser();
+            foreach (var user in employees)
+            {
+
+                appointments = controller.GetUserAppointments(user);
+
+                ListViewItem item = new ListViewItem();
+                item.Text = "";
+                item.SubItems.Add("");
+                item.SubItems.Add("");
+
+                listView1.Items.Add(item);
 
 
+                listView1.Items[0].SubItems[0].Text = user.US_USERNAME;
+
+                foreach (var app in appointments)
+                {
+                    DateTime timeTo = DateTime.Parse(app.APD_TIMETO);
+                    DateTime timeFrom = DateTime.Parse(app.APD_TIMEFROM);
+
+                    TimeSpan totalTime = timeTo.Subtract(timeFrom);
+
+                    totalHours = (int) totalTime.TotalMinutes%60;
+                    availibilityLeft = totalAvailibity - totalHours;
+
+                   
+                    listView1.Items[1].SubItems[2].Text = totalAvailibity.ToString();
+                    listView1.Items[0].SubItems[1].Text = availibilityLeft.ToString();
+                    
+                }
+                for (int i = 0; 1 < employees.Count; i++)
+                {
+                    var u = employees[i];
+                    listView1.Items.Add(u.US_USERNAME);
+                    listView1.Items[i].SubItems.Add(availibilityLeft.ToString());
+                    listView1.Items[i].SubItems.Add(totalAvailibity.ToString());
+                }
+
+            }
 
         }
+
+
+     
+
+        
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -45,9 +131,9 @@ namespace OptikPlanner.View
             {
                 listView1.Columns.Clear();
                 listView1.Columns.Add("Navn", 155);
-                listView1.Columns.Add("Mødte ikke op", 100);
-                listView1.Columns.Add("Opkald", 100);
-
+                listView1.Columns.Add("Tilgængelighed", 100);
+                listView1.Columns.Add("Totaltilgængelighed", 100);
+                
             }
 
         }
