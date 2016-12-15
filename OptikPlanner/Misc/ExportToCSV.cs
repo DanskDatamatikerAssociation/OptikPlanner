039f1d8bada943ecb10a03ee95c45cd2d82d7630
Pilsneren <Parbst2600@gmail.com>
2016-12-15 10:43:46 +0100

    finished exporting option

--- OptikPlanner/Misc/ExportToCSV.cs
+++ OptikPlanner/Misc/ExportToCSV.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace OptikPlanner.Misc
 {
@@ -13,60 +15,34 @@ namespace OptikPlanner.Misc
     {
         public static void ExportToCSV(ListView listView)
         {
-            string logPath2 = @"C:\Users\Danny\Desktop\statistics 02-12-2016.csv";
+            var logPath2 = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.ApplicationData), "statistics " + DateTime.Now.ToShortDateString() + ".csv");
 
-            using (StreamWriter sw = new StreamWriter(new FileStream(logPath2, FileMode.Create, FileAccess.ReadWrite), Encoding.UTF8))
+            DialogResult dialogResult = MessageBox.Show("Er du sikker på at du ønsker at exportere nuværende data til CSV?", "Exportér til CSV", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
             {
-                StringBuilder result = new StringBuilder();
-
-                foreach (ColumnHeader column in listView.Columns)
+                using (StreamWriter sw = new StreamWriter(new FileStream(logPath2, FileMode.Create, FileAccess.Write, FileShare.ReadWrite), Encoding.UTF8))
                 {
-                    result.Append(column.Text + ",");
+                    StringBuilder result = new StringBuilder();
+
+                    foreach (ColumnHeader column in listView.Columns)
+                    {
+                        result.Append(column.Text + ",");
+                    }
+                    result.AppendLine();
+
+                    foreach (ListViewItem listItem in listView.Items)
+                        WriteCSVRow(result, listView.Columns.Count, i => listView.Columns[i].Width > 0, i => listItem.SubItems[i].Text);
+
+
+                    sw.WriteLine(result);
+                    sw.Flush();
+                    sw.Close();
                 }
-                result.AppendLine();
-                
-
-
-                result.AppendLine();
-
-                
-
-                sw.WriteLine(result);
-                sw.Flush();
-                sw.Close();
             }
-
-                //DialogResult dialogResult = MessageBox.Show("Er du sikker på at du ønsker at exportere nuværende data til CSV?", "Exportér til CSV", MessageBoxButtons.YesNo);
-                //if (dialogResult == DialogResult.Yes)
-                //{
-
-                //}
-                //else if (dialogResult == DialogResult.No)
-                //{
-                //}
         }
 
-        public static void ExportToCSV(ListView listView, string filePath, bool includeHidden)
-        {
-            //using (StreamWriter sw = new StreamWriter(new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite), Encoding.UTF8))
-            //{
-            //    StringBuilder result = new StringBuilder();
-            //    result.AppendLine()
-            //}
-            ////make header string
-            //WriteCSVRow(result, listView.Columns.Count, i => includeHidden || listView.Columns[i].Width > 0,
-            //    i => listView.Columns[i].Text);
-
-            ////export data rows
-            //foreach (ListViewItem listItem in listView.Items)
-            //    WriteCSVRow(result, listView.Columns.Count, i => includeHidden || listView.Columns[i].Width > 0,
-            //        i => ((ListViewItem)listItem).SubItems[i].Text);
-
-            //File.WriteAllText(filePath, result.ToString());
-        }
-
-        private static void WriteCSVRow(StringBuilder result, int itemsCount, Func<int, bool> isColumnNeeded,
-            Func<int, string> columnValue)
+        private static void WriteCSVRow(StringBuilder result, int itemsCount, Func<int, bool> isColumnNeeded, Func<int, string> columnValue)
         {
             bool isFirstTime = true;
             for (int i = 0; i < itemsCount; i++)
